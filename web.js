9252fd88c3ce3ba24d37f34cd801d1de082cc615
John Smith <example@stanford.edu>
2013-07-09 21:28:43 +0000

    full path

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
-
+var fs = require('fs')
 var app = express.createServer(express.logger());
-var content = fs.readFileSync("~/bitstarter/index.html", "utf-8");
+var content = fs.readFileSync("/home/ubuntu/bitstarter/index.html", "utf-8");
 
 app.get('/', function(request, response) {
     
-   response.send(buffer.toString("utf-8", content));
+   response.send(buf.toString("utf-8", content));
 });
 
 var port = process.env.PORT || 5000;
