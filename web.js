8b638f38fd6a944e31295d915face60a6177113c
Jason <jasonpweiner@yahoo.com>
2013-07-10 13:59:41 -0400

    Update web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var text = fs.readFileSync('index.html','utf8');
 
 app.get('/', function(request, response) {
     
-   response.send(text);
+   response.send(text.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
