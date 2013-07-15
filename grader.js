4863d51f35f46d82b3edfbbea41da6b1c83e9bc3
John Smith <example@stanford.edu>
2013-07-15 14:27:27 +0000

    update grader.js

--- grader.js
+++ grader.js
@@ -22,10 +22,28 @@ References:
 */
 
 var fs = require('fs');
+var util = require('util');
+var rest = require('restler');
+
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT ="http://www.google.com";//"http://thawing-harbor-2202.herokuapp.com/";
+
+
+var checkUrl = function(url, checksfile){
+    $ = cheerio.load(url);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for (var ii in checks){
+	var present = $(checks[ii]).length >0;
+	out[checks[ii]] = present;
+    }
+    return out;
+};	
+
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +53,7 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -65,7 +84,15 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <URL>', 'URL to parse',clone(assertFileExists), URL_DEFAULT)
         .parse(process.argv);
+  
+if(program.url){
+    rest.get(program.url).on('complete', function(result){
+
+    });
+}
+
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
